d2e835851099ae13b5ddf4969a585d54a302a1e3
winstoncastillo <whinstondj@gmail.com>
2026-02-11 21:21:08 -0500

    Cambios a la automatizacion para programar

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,14 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   allowCypressEnv: false,
   projectId: "z5ejux",
+  
+  // Configuración para videos y screenshots
+  video: true,
+  videoCompression: 32,
+  videosFolder: 'cypress/videos',
+  screenshotOnRunFailure: true,
+  screenshotsFolder: 'cypress/screenshots',
+  trashAssetsBeforeRuns: true,
 
   e2e: {
     setupNodeEvents(on, config) {
